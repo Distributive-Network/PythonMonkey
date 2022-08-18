7fdbc05c8b2c1b73ddcc6e48316e30514ef917e8
John Tedesco <johntedesco@Johns-MacBook-Air.local>
2022-08-18 09:06:17 -0400

    Fixes memory leak bug and adds documentation for the function

--- src/IntType.cpp
+++ src/IntType.cpp
@@ -20,6 +20,11 @@ TYPE IntType::getReturnType() const {
     return this->returnType;
 }
 
+/**
+ * @brief Factors an IntType
+ * 
+ * @return PyObject* which is not referenced by Python garbage collector
+ */
 PyObject* IntType::factor() const {
     PyObject* list = PyList_New(0);
     Py_XINCREF(list);
@@ -37,5 +42,6 @@ PyObject* IntType::factor() const {
     }
     PyList_Sort(list);
 
+    Py_XDECREF(list); 
     return list;
 }
