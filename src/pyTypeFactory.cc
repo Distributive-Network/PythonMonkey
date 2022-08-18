80b49a151af0425b809c70908333c6c2144bbba0
calebaikens <caleb@kingsds.network>
2022-08-18 12:53:43 -0400

    enforce non-optional braces for if statements

--- src/pyTypeFactory.cc
+++ src/pyTypeFactory.cc
@@ -11,16 +11,21 @@
 PyType *pyTypeFactory(PyObject *object) {
   PyType *pyType;
 
-  if (PyLong_Check(object))
+  if (PyLong_Check(object)) {
     pyType = new IntType(object);
-  else if (PyUnicode_Check(object))
+  }
+  else if (PyUnicode_Check(object)) {
     pyType = new StrType(object);
-  else if (PyFunction_Check(object))
+  }
+  else if (PyFunction_Check(object)) {
     pyType = new FuncType(object);
-  else if (PyDict_Check(object))
+  }
+  else if (PyDict_Check(object)) {
     pyType = new DictType(object);
-  else
+  }
+  else {
     return nullptr;
+  }
 
   return pyType;
 }
