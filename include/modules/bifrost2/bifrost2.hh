617cf3e2eaa009636eccd234e6af76825be4c468
calebaikens <caleb@kingsds.network>
2022-09-09 14:56:45 -0400

    add eval function to bifrost2 module

--- include/modules/bifrost2/bifrost2.hh
+++ include/modules/bifrost2/bifrost2.hh
@@ -8,7 +8,36 @@
  * @copyright Copyright (c) 2022
  *
  */
-#ifndef Bifrost_Bifrost2_
-#define Bifrost_Bifrost2_
+#ifndef Bifrost_Module_Bifrost2_
+#define Bifrost_Module_Bifrost2_
+
+#include <jsapi.h>
+#include <js/Initialization.h>
+
+#include <Python.h>
+
+/**
+ * @brief Destroys the JSContext and deletes associated memory. Called when python quits or faces a fatal exception.
+ *
+ * @author Caleb Aikens (caleb@kingsds.network)
+ * @date 2022-09-09
+ */
+static void cleanup();
+
+/**
+ * @brief Function exposed by the python module for evaluating arbitrary JS code
+ *
+ * @param self - Pointer to the module object
+ * @param args - Pointer to the python tuple of arguments (expected to contain JS program as a string as the first element)
+ * @return PyObject* - The result of evaluating the JS program, coerced to a Python type, returned to the python user
+ */
+static PyObject *eval(PyObject *self, PyObject *args);
+
+/**
+ * @brief Initialization function for the module. Starts the JSContext, creates the global object, and sets cleanup functions
+ *
+ * @return PyObject* - The module object to be passed to the python user
+ */
+PyMODINIT_FUNC PyInit_bifrost2(void);
 
 #endif
--- src/modules/bifrost2/bifrost2.cc
+++ src/modules/bifrost2/bifrost2.cc
@@ -1,27 +1,57 @@
 #include "include/modules/bifrost2/bifrost2.hh"
 
+#include "include/StrType.hh"
+
 #include <jsapi.h>
+#include <js/CompilationAndEvaluation.h>
 #include <js/Initialization.h>
+#include <js/SourceText.h>
 
 #include <Python.h>
 
-static JSContext *cx;
+static JSContext *cx;             /**< pointer to Bifrost's JSContext */
+static JS::RootedObject *global;  /**< pointer to the global object of Bifrost's JSContext */
 
 static void cleanup() {
   JS_DestroyContext(cx);
   JS_ShutDown();
+  delete global;
+}
+
+static PyObject *eval(PyObject *self, PyObject *args) {
+
+  StrType *code = new StrType(PyTuple_GetItem(args, 0));
+
+  JSAutoRealm ar(cx, *global);
+  JS::CompileOptions options (cx);
+  options.setFileAndLine("noname", 1);
+
+  JS::SourceText<mozilla::Utf8Unit> source;
+  if (!source.init(cx, code->getValue(), strlen(code->getValue()), JS::SourceOwnership::Borrowed)) {
+    PyErr_SetString(PyExc_RuntimeError, "Spidermonkey could not initialize with given JS code.");
+    return NULL;
+  }
+
+  JS::RootedValue rval(cx);
+  if (!JS::Evaluate(cx, options, source, &rval)) {
+    PyErr_SetString(PyExc_RuntimeError, "Spidermonkey could not evaluate the given JS code.");
+    return NULL;
+  }
+
+  Py_RETURN_NONE;
 }
 
 static PyMethodDef Bifrost2Methods[] = {
+  {"eval", eval, METH_VARARGS, "Javascript evaluator in Python"},
   {NULL, NULL, 0, NULL}
 };
 
 static struct PyModuleDef bifrost2 =
 {
   PyModuleDef_HEAD_INIT,
-  "bifrost2",     /* name of module */
-  "",          /* module documentation, may be NULL */
-  -1,          /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+  "bifrost2",                                   /* name of module */
+  "A module for python to JS interoperability", /* module documentation, may be NULL */
+  -1,                                           /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
   Bifrost2Methods
 };
 
@@ -35,6 +65,12 @@ PyMODINIT_FUNC PyInit_bifrost2(void)
     return NULL;
 
   if (!JS::InitSelfHostedCode(cx))
+    return NULL;
+
+  JS::RealmOptions options;
+  static JSClass globalClass = {"global", JSCLASS_GLOBAL_FLAGS, &JS::DefaultGlobalClassOps};
+  global = new JS::RootedObject(cx, JS_NewGlobalObject(cx, &globalClass, nullptr, JS::FireOnNewGlobalHook, options));
+  if (!global)
     return NULL;
 
   Py_AtExit(cleanup);
