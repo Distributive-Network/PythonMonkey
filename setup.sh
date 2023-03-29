e81eb5c2fbd91c3dc702bb860ef682fdbc4b1aff
Caleb Aikens <caleb@distributive.network>
2023-03-29 08:15:39 -0400

    add sudo inside setup.sh, update README

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 # Get number of CPU cores
 CPUS=$(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1)
 
-apt-get update --yes
-apt-get upgrade --yes
-apt-get install cmake python3-dev python3-pytest doxygen graphviz gcovr llvm g++ pkg-config m4 --yes
-curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y #install rust compiler
+sudo apt-get update --yes
+sudo apt-get upgrade --yes
+sudo apt-get install cmake python3-dev python3-pytest doxygen graphviz gcovr llvm g++ pkg-config m4 --yes
+sudo curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y #install rust compiler
 wget -q https://ftp.mozilla.org/pub/firefox/releases/102.2.0esr/source/firefox-102.2.0esr.source.tar.xz
 tar xf firefox-102.2.0esr.source.tar.xz
 cd firefox-102.2.0/js
@@ -17,4 +17,4 @@ mkdir _build
 cd _build
 ../configure --disable-jemalloc --with-system-zlib --with-intl-api --enable-optimize
 make -j$CPUS
-make install
+sudo make install
