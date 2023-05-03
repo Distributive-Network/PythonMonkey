bd6ab2f4a5e8208919cf799cfe0803cc681c2179
Tom Tang <xmader@distributive.network>
2023-05-03 18:54:32 +0000

    chore: install numpy for our tests

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ CPUS=$(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/
 
 sudo apt-get update --yes
 sudo apt-get upgrade --yes
-sudo apt-get install cmake python3-dev python3-pytest doxygen graphviz gcovr llvm g++ pkg-config m4 --yes
+sudo apt-get install cmake python3-dev python3-pytest python3-numpy doxygen graphviz gcovr llvm g++ pkg-config m4 --yes
 sudo curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y #install rust compiler
 wget -q https://ftp.mozilla.org/pub/firefox/releases/102.2.0esr/source/firefox-102.2.0esr.source.tar.xz
 tar xf firefox-102.2.0esr.source.tar.xz
