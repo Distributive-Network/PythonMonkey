de50c5d5dc238a864131700d8925dc4f9f4681f2
Tom Wenzheng Tang <xmader@distributive.network>
2023-05-30 18:21:11 -0400

    chore: update test_script.sh to symlink .so file into the package

--- test_script.sh
+++ test_script.sh
@@ -3,6 +3,8 @@ cmake ..
 cmake --build .
 cd tests
 ctest --rerun-failed --output-on-failure
+cd ../../python/pythonmonkey
+ln -sf ../../build/src/pythonmonkey.so .
 cd ../../tests/python
 ln -sf ../../python/pythonmonkey .
 python -m pytest .
