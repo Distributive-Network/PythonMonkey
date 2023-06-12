2cbce6392122d6c31e7c43e9ddcbe5f3a52d553f
Tom Tang <xmader@distributive.network>
2023-06-12 22:55:25 +0000

    chore: build source distribution (sdist) file to allow install-from-source

--- build.py
+++ build.py
@@ -15,7 +15,17 @@ def execute(cmd: str):
     if return_code:
         raise subprocess.CalledProcessError(return_code, cmd)
 
+def ensure_spidermonkey():
+    # Check if SpiderMonkey libs already exist
+    spidermonkey_lib_exist = os.path.exists("./_spidermonkey_install/lib")
+    if spidermonkey_lib_exist:
+        return
+
+    # Build SpiderMonkey
+    execute("bash ./setup.sh")
+
 def build():
+    ensure_spidermonkey()
     build_script_sh = os.path.join( dir_path, 'build_script.sh' )
     execute(f"bash {build_script_sh}")
     execute("cp ./build/src/pythonmonkey.so ./python/pythonmonkey/")
