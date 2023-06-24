ed09e2a69acc412cf8e699ec1dbb584931429fe7
Wes Garland <wes@distributive.network>
2023-06-23 23:53:02 -0400

    Add python-language CommonJS module test

---
+++ tests/js/modules/python-cjs-module.py
@@ -0,0 +1,9 @@
+# @file         python-cjs-module.py
+#               A CommonJS Module written Python.
+# @author       Wes Garland, wes@distributive.network
+# @date         Jun 2023
+
+def helloWorld():
+  print('hello, world!')
+
+exports['helloWorld'] = helloWorld
