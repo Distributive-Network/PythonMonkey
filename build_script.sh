01d34e234bb9a56322b1a988fa30b298941ae0b0
Wes Garland <wes@distributive.network>
2023-06-23 20:35:33 -0400

    Generate __version__ directly from pyproject.toml at build time

--- build_script.sh
+++ build_script.sh
@@ -1,3 +1,12 @@
+#! /bin/bash
+#
+# @file         build.sh
+# @author       Giovanni Tedesco <giovanni@distributive.network>
+# @date         Aug 2022
+
+cd `dirname "$0"`
+topDir=`pwd`
+
 # Get number of CPU cores
 CPUS=$(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1)
 
@@ -10,7 +19,11 @@ fi
 cd build
 cmake ..
 cmake --build . -j$CPUS
-cp -f ../build/src/pythonmonkey.so ../python/pythonmonkey/
 
-# npm is used to load JS components, see package.json
-npm i
+cd "$topDir"
+npm i # npm is used to load JS components, see package.json
+cp -f build/src/pythonmonkey.so python/pythonmonkey/
+grep '^version' pyproject.toml \
+| head -1 \
+| sed 's/^version /__version__ /' \
+> python/pythonmonkey/version.py
--- python/pythonmonkey/__init__.py
+++ python/pythonmonkey/__init__.py
@@ -1,2 +1,6 @@
 from .pythonmonkey import *
-__version__ = '0.1.0'
+#from .__version__ import *
+import os
+exec(open(os.path.dirname(__file__) + '/version.py').read())
+#import importlib.metadata
+#__version__= importlib.metadata.version(__name__)
