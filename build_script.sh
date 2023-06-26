da3a9006532bff8af40aa7960de7f08af2d1e960
Wes Garland <wes@distributive.network>
2023-06-26 14:03:50 -0400

    build_script.sh - stop growing version.py

--- build_script.sh
+++ build_script.sh
@@ -22,12 +22,13 @@ cmake --build . -j$CPUS
 
 cd "${topDir}"
 cp -f build/src/pythonmonkey.so python/pythonmonkey/
-echo "# This file was generated via $0 by `id -un` on `date` - do not edit by hand!"
-grep '^version' pyproject.toml \
-| head -1 \
-| sed 's/^version /__version__ /' \
->> python/pythonmonkey/version.py
+(
+  echo "# This file was generated via $0 by `id -un` on `hostname` at `date` - do not edit by hand!"
+  grep '^version' pyproject.toml \
+  | head -1 \
+  | sed 's/^version /__version__ /' \
+) > python/pythonmonkey/version.py
 
 # npm is used to load JS components, see package.json
 cd "${topDir}/python/pythonmonkey/"
-npm i 
+npm i
