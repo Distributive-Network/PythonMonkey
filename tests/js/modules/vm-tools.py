4c11666be7f421e90faecca56eae58d0438c3356
Wes Garland <wes@distributive.network>
2023-06-28 11:47:34 -0400

    pmjs, require examples, etc - let poetry manage sys.path to find pythonmonkey.so

--- tests/js/modules/vm-tools.py
+++ tests/js/modules/vm-tools.py
@@ -4,8 +4,6 @@
 # @author       Wes Garland, wes@distributive.network
 # @date         Jun 2023
 
-import sys, os
-sys.path.append(os.path.dirname(__file__) + '/../../../python') # location of pythonmonkey module
 import pythonmonkey as pm
 
 exports['isCompilableUnit'] = pm.isCompilableUnit
