a2f1477525457fd1cf0d560c7a3b434ec1308c90
Wes Garland <wes@distributive.network>
2023-06-29 11:23:17 -0400

    pmjs - fix ^C handling in REPL, get correct require semantics for program modules and extra-module environment

---
+++ pmjs-lib/program-init.js
@@ -0,0 +1,6 @@
+/**
+ * @file        program=init.js
+ *              Initialize the global context specifically for running JS programs
+ */
+globalThis.require = require;
+globalThis.module = module;
