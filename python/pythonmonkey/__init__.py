eb42e8f321f03f425378a224ccbe8ed08fa5af5a
Tom Tang <xmader@distributive.network>
2023-06-29 21:56:44 +0000

    refactor(console): move to files to the `builtin_modules` directory

--- python/pythonmonkey/__init__.py
+++ python/pythonmonkey/__init__.py
@@ -5,6 +5,3 @@ from .require import *
 # Expose the package version
 import importlib.metadata
 __version__= importlib.metadata.version(__name__)
-
-# Install JS/Python implemented browser/Node.js APIs
-from . import js_apis
--- python/pythonmonkey/js_apis/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .. import require as pm
-
-require = pm.createRequire(__file__)
-require("./console.js")
