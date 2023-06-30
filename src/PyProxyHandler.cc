67dad59fb764243e893121d3e30319b775a583fc
Tom Tang <xmader@distributive.network>
2023-06-30 21:59:35 +0000

    docs(PyProxyHandler): use Python return `NotImplemented` for JS symbol type property keys

--- src/PyProxyHandler.cc
+++ src/PyProxyHandler.cc
@@ -25,7 +25,8 @@
 PyObject *idToKey(JSContext *cx, JS::HandleId id) {
   if (id.isSymbol() || id.isVoid()) {
     // TODO (Tom Tang): Revisit this once we have Symbol coecrion support
-    Py_RETURN_NOTIMPLEMENTED;
+    // FIXME (Tom Tang): key collision for any JS symbol
+    Py_RETURN_NOTIMPLEMENTED; // OK to be used as Python dict key because Python `NotImplemented` is hashable 
   }
 
   JS::RootedValue idv(cx, js::IdToValue(id));
