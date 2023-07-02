211b9e637caaaad2a600ed9e8a9d98f8d6dbdc3f
Tom Tang <xmader@distributive.network>
2023-07-02 13:31:15 +0000

    fix(JSObjectProxy): initialize jsObject root

--- src/JSObjectProxy.cc
+++ src/JSObjectProxy.cc
@@ -46,7 +46,7 @@ void JSObjectProxyMethodDefinitions::JSObjectProxy_dealloc(JSObjectProxy *self)
 PyObject *JSObjectProxyMethodDefinitions::JSObjectProxy_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
   PyObject *self = PyDict_Type.tp_new(&JSObjectProxyType, args, kwds);
-  ((JSObjectProxy *)self)->jsObject.set(JS_NewObject(GLOBAL_CX, NULL));
+  ((JSObjectProxy *)self)->jsObject = JS::RootedObject(GLOBAL_CX);
   return self;
 }
 
