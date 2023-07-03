e456181ab44650ecb71e2036a6e17ee19efb66eb
Wes Garland <wes@distributive.network>
2023-07-03 00:46:58 -0400

    Support lastest mozilla-central jsapi from foreign repo [WIP]

--- cmake/modules/FindSpiderMonkey.cmake
+++ cmake/modules/FindSpiderMonkey.cmake
@@ -1,87 +1,111 @@
 #
-#	CMake Find Mozilla SpiderMonkey JavaScript Engine by Parra Studios
-#	CMake script to find Mozilla SpiderMonkey Javascript Engine.
+#       CMake Find Mozilla SpiderMonkey JavaScript Engine by Parra Studios
+#       CMake script to find Mozilla SpiderMonkey Javascript Engine.
 #
-#	Copyright (C) 2016 - 2022 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
+#       Copyright (C) 2016 - 2022 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
 #
-#	Licensed under the Apache License, Version 2.0 (the "License");
-#	you may not use this file except in compliance with the License.
-#	You may obtain a copy of the License at
+#       Licensed under the Apache License, Version 2.0 (the "License");
+#       you may not use this file except in compliance with the License.
+#       You may obtain a copy of the License at
 #
-#		http://www.apache.org/licenses/LICENSE-2.0
+#               http://www.apache.org/licenses/LICENSE-2.0
 #
-#	Unless required by applicable law or agreed to in writing, software
-#	distributed under the License is distributed on an "AS IS" BASIS,
-#	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#	See the License for the specific language governing permissions and
-#	limitations under the License.
+#       Unless required by applicable law or agreed to in writing, software
+#       distributed under the License is distributed on an "AS IS" BASIS,
+#       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#       See the License for the specific language governing permissions and
+#       limitations under the License.
 #
 
+# Two modes of operation are supported; this is selected when PythonMokeys' setup.sh is run.
+# Setup communicates the mode (and other variables for non-automatic mode) by generating
+# SpiderMonkeySetup.cmake.
+#
+# Automatic Mode (default)
+# ------------------------
 # Find the SpiderMonkey header files and libraries
+# - finds it where the setup.sh script downloads it to
 #
-#  SPIDERMONKEY_INCLUDE_DIR	- where to find jsapi.h, etc.
-#  SPIDERMONKEY_LIBRARY		- List of libraries when using libjs.
-#  SPIDERMONKEY_FOUND		  - True if SpiderMonkey found.
-#  SPIDERMONKEY_THREADSAFE	 - True if SpiderMonkey is compiled with multi threading support.
+# Mozilla-Repo Mode
+# -----------------
+# - set via setup.sh --with-mozilla=/path/to/hg/mozilla-central
+# - expects that the library is already built
+# - copies the library over at build time so that it can wind up in the wheel, get proper rpath, etc
+# 
+#
+# SPIDERMONKEY_INCLUDE_DIR     - where to find jsapi.h, etc.
+# SPIDERMONKEY_LIBRARY         - List of libraries when using libjs.
+# SPIDERMONKEY_FOUND           - True if SpiderMonkey found.
+# SPIDERMONKEY_THREADSAFE      - True if SpiderMonkey is compiled with multi threading support.
 
-#Last Change: 2022-10-03 (Caleb Aikens)
-
+include(SpiderMonkeySetup)
 include(CheckIncludeFileCXX)
 include(CheckCXXSourceCompiles)
 include(CheckCXXSourceRuns)
 include(FindPackageHandleStandardArgs)
 
-# Prevent vervosity if already included
+# Prevent verbosity if already included
 if(SPIDERMONKEY_FOUND)
-	set(SPIDERMONKEY_FIND_QUIETLY TRUE)
+        set(SPIDERMONKEY_FIND_QUIETLY TRUE)
 endif()
 
-# SpiderMonkey search paths
-set(SPIDERMONKEY_PATHS
-  "${CMAKE_CURRENT_SOURCE_DIR}/_spidermonkey_install"
-  "${CMAKE_CURRENT_SOURCE_DIR}/_spidermonkey_install/lib"
-  ${SPIDERMONKEY_ROOT}
-	$ENV{SPIDERMONKEY_ROOT}
-	~/Library/Frameworks
-	/Library/Frameworks
-  /usr/local/lib
-	/usr/local
-	/usr
-	/sw # Fink
-	/opt/local # DarwinPorts
-	/opt/csw # Blastwave
-	/opt
-	/usr/freeware
-  C:/mozilla-source/mozilla-unified/obj-x86_64-pc-mingw32 # Windows Mozilla bootstrap build
-)
+message("---------------------------------------------------------------------------------")
+if(DEFINED MOZILLA)
+  message(" * FindSpiderMonkey - Running in Mozilla-Repo mode")
+  message(" . ${MOZILLA} contains SpiderMonkey version ${LIBMOZJS_VERSION}")
+  set(SPIDERMONKEY_LIBRARY ${LIBMOZJS})
+  set(SPIDERMONKEY_INCLUDE_DIR ${MOZJS_INCLUDE})
+  file(COPY ${LIBMOZJS_ORIGIN_PATH} DESTINATION python/pythonmonkey/${LIBMOZJS})
+  message("${LIBMOZJS_ORIGIN_PATH} and ${LIBMOZJS}")
+else()
+  message("FindSpiderMonkey - Running in auto-detect mode")
+endif()
 
-# SpiderMonkey headers
 set(SPIDERMONKEY_HEADERS jsapi.h js/RequiredDefines.h)
-
-# SpiderMonkey include suffix paths
-set(SPIDERMONKEY_INCLUDE_SUFFIX_PATHS dist/include js/src include include/js include/mozjs-48a1 include/mozjs-102/)
-
-# Find SpiderMonkey include path
-find_path(SPIDERMONKEY_INCLUDE_DIR ${SPIDERMONKEY_HEADERS}
-  PATHS ${SPIDERMONKEY_PATHS}
-  PATH_SUFFIXES ${SPIDERMONKEY_INCLUDE_SUFFIX_PATHS}
-  DOC "Mozilla SpiderMonkey JavaScript Engine Headers"
-  NO_DEFAULT_PATH
-)
-
-# SpiderMonkey libs
-set(SPIDERMONKEY_LIBRARY_NAMES libmozjs-102.so libmozjs-102.dylib mozjs-102.lib)
-
+set(SPIDERMONKEY_INCLUDE_SUFFIX_PATHS dist/include js/src include include/js)
 set(SPIDERMONKEY_LIB_SUFFIX_PATHS js/src/build lib)
 
-# Find SpiderMonkey base library
-find_library(SPIDERMONKEY_LIBRARY
-  NAMES ${SPIDERMONKEY_LIBRARY_NAMES}
-  PATHS ${SPIDERMONKEY_PATHS}
-  PATH_SUFFIXES ${SPIDERMONKEY_LIB_SUFFIX_PATHS}
-  DOC "Mozilla SpiderMonkey JavaScript Engine Library"
-)
+if(NOT DEFINED MOZILLA)
+  # SpiderMonkey search paths
+  set(SPIDERMONKEY_PATHS
+        "${CMAKE_CURRENT_SOURCE_DIR}/_spidermonkey_install"
+        "${CMAKE_CURRENT_SOURCE_DIR}/_spidermonkey_install/lib"
+        ${SPIDERMONKEY_ROOT}
+        $ENV{SPIDERMONKEY_ROOT}
+        ~/Library/Frameworks
+        /Library/Frameworks
+        /usr/local/lib
+        /usr/local
+        /usr
+        /sw # Fink
+        /opt/local # DarwinPorts
+        /opt/csw # Blastwave
+        /opt
+        /usr/freeware
+        C:/mozilla-source/mozilla-unified/obj-x86_64-pc-mingw32 # Windows Mozilla bootstrap build
+  )
 
+  set(SPIDERMONKEY_INCLUDE_SUFFIX_PATHS ${SPIDERMONKEY_INCLUDE_SUFFIX_PATHS} include/mozjs-48a1 include/mozjs-102/)
+
+  # Find SpiderMonkey include path
+  find_path(SPIDERMONKEY_INCLUDE_DIR ${SPIDERMONKEY_HEADERS}
+    PATHS ${SPIDERMONKEY_PATHS}
+    PATH_SUFFIXES ${SPIDERMONKEY_INCLUDE_SUFFIX_PATHS}
+    DOC "Mozilla SpiderMonkey JavaScript Engine Headers"
+    NO_DEFAULT_PATH
+  )
+
+# SpiderMonkey libs
+  set(SPIDERMONKEY_LIBRARY_NAMES libmozjs-102.so libmozjs-102.dylib mozjs-102.lib)
+
+  # Find SpiderMonkey base library
+  find_library(SPIDERMONKEY_LIBRARY
+    NAMES ${SPIDERMONKEY_LIBRARY_NAMES}
+    PATHS ${SPIDERMONKEY_PATHS}
+    PATH_SUFFIXES ${SPIDERMONKEY_LIB_SUFFIX_PATHS}
+    DOC "Mozilla SpiderMonkey JavaScript Engine Library"
+  )
+endif()
 set(SPIDERMONKEY_LIBRARIES ${SPIDERMONKEY_LIBRARY})
 
 list(APPEND CMAKE_REQUIRED_INCLUDES ${SPIDERMONKEY_INCLUDE_DIR})
@@ -89,68 +113,68 @@ list(APPEND CMAKE_REQUIRED_DEFINITIONS ${SPIDERMONKEY_DEFINITIONS})
 list(APPEND CMAKE_REQUIRED_LIBRARIES ${SPIDERMONKEY_LIBRARY})
 
 check_cxx_source_compiles(
-	"#include <jsapi.h>
-	int main()
-	{
+        "#include <jsapi.h>
+        int main()
+        {
     JSRuntime *rt = JS_NewRuntime(8L * 1024L * 1024L);
-		if (rt != NULL)
-		{
-		return 0;
+                if (rt != NULL)
+                {
+                return 0;
   }
   return 1;
-	}"
+        }"
 
-	SPIDERMONKEY_BUILDS
+        SPIDERMONKEY_BUILDS
 )
 
 find_package_handle_standard_args(SpiderMonkey DEFAULT_MSG SPIDERMONKEY_LIBRARIES SPIDERMONKEY_INCLUDE_DIR) # SPIDERMONKEY_BUILDS)
 
 if(SPIDERMONKEY_FOUND)
-	set(CMAKE_REQUIRED_INCLUDES ${SPIDERMONKEY_INCLUDE_DIR})
-	set(CMAKE_REQUIRED_DEFINITIONS ${SPIDERMONKEY_DEFINITIONS})
-	set(CMAKE_REQUIRED_LIBRARIES ${SPIDERMONKEY_LIBRARY})
+        set(CMAKE_REQUIRED_INCLUDES ${SPIDERMONKEY_INCLUDE_DIR})
+        set(CMAKE_REQUIRED_DEFINITIONS ${SPIDERMONKEY_DEFINITIONS})
+        set(CMAKE_REQUIRED_LIBRARIES ${SPIDERMONKEY_LIBRARY})
 
 
-	find_path(SPIDERMONKEY_JS_CONFIG_HEADER_PATH js-config.h
-		PATHS ${SPIDERMONKEY_PATHS}
-		PATH_SUFFIXES ${SPIDERMONKEY_INCLUDE_SUFFIX_PATHS}
-		DOC "Mozilla SpiderMonkey JavaScript Engine Config Header"
-	)
+        find_path(SPIDERMONKEY_JS_CONFIG_HEADER_PATH js-config.h
+                PATHS ${SPIDERMONKEY_PATHS}
+                PATH_SUFFIXES ${SPIDERMONKEY_INCLUDE_SUFFIX_PATHS}
+                DOC "Mozilla SpiderMonkey JavaScript Engine Config Header"
+        )
 
-	check_include_file_cxx("${SPIDERMONKEY_JS_CONFIG_HEADER_PATH}/js-config.h" SPIDERMONKEY_JS_CONFIG_HEADER)
+        check_include_file_cxx("${SPIDERMONKEY_JS_CONFIG_HEADER_PATH}/js-config.h" SPIDERMONKEY_JS_CONFIG_HEADER)
 
-	if(NOT SPIDERMONKEY_JS_CONFIG_HEADER)
-		check_cxx_source_runs(
-			"#include <stdio.h>
-			extern \"C\" void js_GetCurrentThread();
-			int main()
-			{
-				printf(\"%p\", (void *)js_GetCurrentThread);
-				return ((void *)js_GetCurrentThread != NULL) ? 0 : 1;
-			}"
+        if(NOT SPIDERMONKEY_JS_CONFIG_HEADER)
+                check_cxx_source_runs(
+                        "#include <stdio.h>
+                        extern \"C\" void js_GetCurrentThread();
+                        int main()
+                        {
+                                printf(\"%p\", (void *)js_GetCurrentThread);
+                                return ((void *)js_GetCurrentThread != NULL) ? 0 : 1;
+                        }"
 
-			SPIDERMONKEY_THREADSAFE
-		)
+                        SPIDERMONKEY_THREADSAFE
+                )
 
-		if(SPIDERMONKEY_THREADSAFE)
-			set(SPIDERMONKEY_DEFINITIONS ${SPIDERMONKEY_DEFINITIONS} -DJS_THREADSAFE)
-		endif()
-	else()
-		file(STRINGS "${SPIDERMONKEY_JS_CONFIG_HEADER_PATH}/js-config.h" JS_THREADSAFE REGEX "^#define JS_THREADSAFE [^\"]*$")
+                if(SPIDERMONKEY_THREADSAFE)
+                        set(SPIDERMONKEY_DEFINITIONS ${SPIDERMONKEY_DEFINITIONS} -DJS_THREADSAFE)
+                endif()
+        else()
+                file(STRINGS "${SPIDERMONKEY_JS_CONFIG_HEADER_PATH}/js-config.h" JS_THREADSAFE REGEX "^#define JS_THREADSAFE [^\"]*$")
 
-		if(${JS_THREADSAFE} MATCHES "1")
-			set(SPIDERMONKEY_THREADSAFE "TRUE")
-		endif()
-	endif()
+                if(${JS_THREADSAFE} MATCHES "1")
+                        set(SPIDERMONKEY_THREADSAFE "TRUE")
+                endif()
+        endif()
 
-	# Removed in newest versions of SpiderMonkey
-	#if(NOT SPIDERMONKEY_THREADSAFE)
-	#	message(FATAL_ERROR "SpiderMonkey must be built with JS_THREADSAFE")
-	#endif()
+        # Removed in newest versions of SpiderMonkey
+        #if(NOT SPIDERMONKEY_THREADSAFE)
+        #       message(FATAL_ERROR "SpiderMonkey must be built with JS_THREADSAFE")
+        #endif()
 
-	get_filename_component(SPIDERMONKEY_LIBDIR ${SPIDERMONKEY_LIBRARY} PATH)
+        get_filename_component(SPIDERMONKEY_LIBDIR ${SPIDERMONKEY_LIBRARY} PATH)
 
-	link_directories(${SPIDERMONKEY_LIBDIR})
+        link_directories(${SPIDERMONKEY_LIBDIR})
 endif()
 
 list(REMOVE_ITEM CMAKE_REQUIRED_LIBRARIES ${SPIDERMONKEY_LIBRARY})
@@ -158,3 +182,4 @@ list(REMOVE_DUPLICATES CMAKE_REQUIRED_LIBRARIES)
 
 mark_as_advanced(SPIDERMONKEY_INCLUDE_DIR SPIDERMONKEY_LIBRARY)
 mark_as_advanced(SPIDERMONKEY_JS_CONFIG_HEADER_PATH)
+message("---------------------------------------------------------------------------------")
--- setup.sh
+++ setup.sh
@@ -1,8 +1,68 @@
 #!/bin/bash
+# @file         setup.sh
+#               Setup tasks for a PythonMonkey build, including locating and building the SpiderMonkey source code.
+# @author       Caleb Aikens <caleb@distributive.network>
+# @date         Feb 2023
+
 set -euo pipefail
-IFS=$'\n\t'
+cd `dirname "$0"`
+topDir="`pwd`"
 
+usage()
+{
+  cat <<EOHELP
 
+$0 - Setup build environment for PythonMonkey
+Copyright (c) 2023 Distributive. Released under the terms of MIT License.
+
+Usage: $0 [[--options] ... ]
+Where:
+  --help                        Shows this help
+  --with-mozilla=<dir>          Sets the location of the mozilla source repo
+  -with-build=<debug|release>   Sets the build type
+EOHELP
+}
+
+panic()
+{
+  echo "$*" >&2
+  exit 3
+}
+
+while getopts "dh-:" OPTION; do
+  if [ "$OPTION" = "-" ]; then
+    if [[ "$OPTARG" =~ (^[a-z0-9-]+)=(.*) ]]; then
+      OPTION="${BASH_REMATCH[1]}"
+      OPTARG="${BASH_REMATCH[2]}"
+    else
+      OPTION="${OPTARG}"
+      OPTARG=""
+    fi
+  fi
+  case $OPTION in
+    h|help)
+      usage
+      exit 1
+      ;;
+    "with-build")
+      [ "${PYTHONMONKEY_BUILD:-}" != "${OPTARG}" ] && echo "Setting PythonMonkey build type to ${OPTARG}"
+      PYTHONMONKEY_BUILD="${OPTARG}"      
+      ;;
+    "with-mozilla")
+      eval MOZILLA=`echo "${OPTARG}"`
+      if [ ! -d "${MOZILLA}" ]; then
+        echo "Error: ${MOZILLA} is not a directory" >&2
+        exit 2
+      fi
+      "${topDir}/parse-moz-environment" "${MOZILLA}" || panic "Error running ${MOZILLA}/mach"
+      ;;
+    *)
+      echo "Unrecognized option: $OPTION"
+      exit 2
+      ;;
+  esac
+done
+    
 # Get number of CPU cores
 CPUS=$(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1)
 
@@ -26,44 +86,57 @@ curl -sSL https://install.python-poetry.org | python3 - --version "1.5.1"
 poetry self add "poetry-dynamic-versioning[plugin]"
 echo "Done installing dependencies"
 
-echo "Downloading spidermonkey source code"
-wget -c -q https://ftp.mozilla.org/pub/firefox/releases/102.11.0esr/source/firefox-102.11.0esr.source.tar.xz
-mkdir -p firefox-source
-tar xf firefox-102.11.0esr.source.tar.xz -C firefox-source --strip-components=1 # strip the root folder
-echo "Done downloading spidermonkey source code"
+if [ "${MOZILLA:-}" != "" ]; then
+  echo "Using spidermonkey in ${MOZILLA}"
+  "${topDir}"/parse-moz-environment "${MOZILLA}" || exit $?
+  if [ "${MOZCONFIG:-}" = "" ]; then
+    echo "Warning: MOZCONFIG not set - not building ${MOZILLA} (you can build it later)"
+  else
+    echo " - building SpiderMonkey in ${MOZILLA}"
+    ${MOZILLA}/mach build | grep -v '^To take your build for a test drive'
+  fi
+else
+  echo "Downloading spidermonkey source code"
+  wget -c -q https://ftp.mozilla.org/pub/firefox/releases/102.11.0esr/source/firefox-102.11.0esr.source.tar.xz
+  mkdir -p firefox-source
+  tar xf firefox-102.11.0esr.source.tar.xz -C firefox-source --strip-components=1 # strip the root folder
+  echo "Done downloading spidermonkey source code"
 
-echo "Building spidermonkey"
-cd firefox-source
-# making it work for both GNU and BSD (macOS) versions of sed
-sed -i'' -e 's/os not in ("WINNT", "OSX", "Android")/os not in ("WINNT", "Android")/' ./build/moz.configure/pkg.configure # use pkg-config on macOS
-sed -i'' -e '/"WindowsDllMain.cpp"/d' ./mozglue/misc/moz.build # https://discourse.mozilla.org/t/105671, https://bugzilla.mozilla.org/show_bug.cgi?id=1751561
-sed -i'' -e '/"winheap.cpp"/d' ./memory/mozalloc/moz.build # https://bugzilla.mozilla.org/show_bug.cgi?id=1802675
-sed -i'' -e 's/bool Unbox/JS_PUBLIC_API bool Unbox/g' ./js/public/Class.h           # need to manually add JS_PUBLIC_API to js::Unbox until it gets fixed in Spidermonkey
-sed -i'' -e 's/bool js::Unbox/JS_PUBLIC_API bool js::Unbox/g' ./js/src/vm/JSObject.cpp  # same here
-cd js/src
-cp ./configure.in ./configure
-chmod +x ./configure
-mkdir -p _build
-cd _build
-mkdir -p ../../../../_spidermonkey_install/
-../configure \
-  --prefix=$(realpath $PWD/../../../../_spidermonkey_install) \
-  --with-intl-api \
-  --without-system-zlib \
-  --disable-debug-symbols \
-  --disable-jemalloc \
-  --disable-tests \
-  --enable-optimize 
-make -j$CPUS
-echo "Done building spidermonkey"
+  echo "Building spidermonkey"
+  cd firefox-source
+  # making it work for both GNU and BSD (macOS) versions of sed
+  sed -i'' -e 's/os not in ("WINNT", "OSX", "Android")/os not in ("WINNT", "Android")/' ./build/moz.configure/pkg.configure # use pkg-config on macOS
+  sed -i'' -e '/"WindowsDllMain.cpp"/d' ./mozglue/misc/moz.build # https://discourse.mozilla.org/t/105671, https://bugzilla.mozilla.org/show_bug.cgi?id=1751561
+  sed -i'' -e '/"winheap.cpp"/d' ./memory/mozalloc/moz.build # https://bugzilla.mozilla.org/show_bug.cgi?id=1802675
+  sed -i'' -e 's/bool Unbox/JS_PUBLIC_API bool Unbox/g' ./js/public/Class.h           # need to manually add JS_PUBLIC_API to js::Unbox until it gets fixed in Spidermonkey
+  sed -i'' -e 's/bool js::Unbox/JS_PUBLIC_API bool js::Unbox/g' ./js/src/vm/JSObject.cpp  # same here
+  cd js/src
+  cp ./configure.in ./configure
+  chmod +x ./configure
+  mkdir -p _build
+  cd _build
+  mkdir -p ../../../../_spidermonkey_install/
+  ../configure \
+    --prefix=$(realpath $PWD/../../../../_spidermonkey_install) \
+    --with-intl-api \
+    --without-system-zlib \
+    --disable-debug-symbols \
+    --disable-jemalloc \
+    --disable-tests \
+    --enable-optimize 
+  make -j$CPUS
+  echo "Done building spidermonkey"
 
-echo "Installing spidermonkey"
-# install to ../../../../_spidermonkey_install/
-make install 
-cd ../../../../_spidermonkey_install/lib/
-if [[ "$OSTYPE" == "darwin"* ]]; then # macOS
-  # Set the `install_name` field to use RPATH instead of an absolute path
-  # overrides https://hg.mozilla.org/releases/mozilla-esr102/file/89d799cb/js/src/build/Makefile.in#l83
-  install_name_tool -id @rpath/$(basename ./libmozjs*) ./libmozjs* # making it work for whatever name the libmozjs dylib is called
+  echo "Installing spidermonkey"
+  # install to ../../../../_spidermonkey_install/
+  make install 
+  cd ../../../../_spidermonkey_install/lib/
+  if [[ "$OSTYPE" == "darwin"* ]]; then # macOS
+    # Set the `install_name` field to use RPATH instead of an absolute path
+    # overrides https://hg.mozilla.org/releases/mozilla-esr102/file/89d799cb/js/src/build/Makefile.in#l83
+    install_name_tool -id @rpath/$(basename ./libmozjs*) ./libmozjs* # making it work for whatever name the libmozjs dylib is called
+  fi
+  echo "Done installing spidermonkey"
 fi
-echo "Done installing spidermonkey"
+
+echo "Done setting up PythonMonkey; you can now run ./build_script.sh"
