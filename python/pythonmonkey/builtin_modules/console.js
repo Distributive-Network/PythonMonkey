7a6e621514ac2dad27c447cdccd0f3ae4f6b01f6
Wes Garland <wes@distributive.network>
2023-07-06 11:11:47 -0400

    Add stdout/stderr support to temp console module

--- python/pythonmonkey/builtin_modules/console.js
+++ python/pythonmonkey/builtin_modules/console.js
@@ -4,16 +4,28 @@
  * @author   Wes Garland, wes@distributive.network
  * @date     June 2023
  */
-function Console(print)
+function Console(stdout, stderr)
 {
-  this.log   = print;
-  this.debug = print;
-  this.error = print;
-  this.warn  = print;
-  this.info  = print;
+  function write(stream, args)
+  {
+    stream.write(args.join(' ') + '\n');
+  }
+    
+  if (typeof stdout === 'object' && !stdout.write)
+  {
+    /* handle overload, eg new Console(process) */
+    stderr = stdout.stderr;
+    stdout = stdout.stdout;
+  }
+
+  this.log   = (...args) => write(stdout, args);
+  this.debug = (...args) => write(stdout, args);
+  this.error = (...args) => write(stderr, args);
+  this.warn  = (...args) => write(stderr, args);
+  this.info  = (...args) => write(stdout, args);
 }
 
 if (!globalThis.console)
-  globalThis.console = new Console(python.print);
+  globalThis.console = new Console(python)
 
 exports.Console = Console;
