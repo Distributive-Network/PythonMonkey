9d2c24bf90213159bf8348fc18195cedcfba71d0
Tom Tang <xmader@distributive.network>
2023-07-12 17:59:15 +0000

    feat(event-loop): support passing a `code` string to `setTimeout` as the callback function

--- python/pythonmonkey/builtin_modules/timers.js
+++ python/pythonmonkey/builtin_modules/timers.js
@@ -21,8 +21,9 @@ const {
  */
 function setTimeout(handler, delayMs = 0, ...args) {
   // Ensure the first parameter is a function
+  // We support passing a `code` string to `setTimeout` as the callback function
   if (typeof handler !== "function") {
-    throw new TypeError("The first parameter to setTimeout() is not a function")
+    handler = new Function(handler)
   }
 
   // `setTimeout` allows passing additional arguments to the callback, as spec-ed
