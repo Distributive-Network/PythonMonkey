fb8b5789c691d282a581dd42298534d4606a3eec
Tom Tang <xmader@distributive.network>
2023-07-21 07:19:36 +0000

    refactor(datetime): revert back to use `PyDateTime_FromDateAndTime` because on Windows the timestamp is restricted to years from 1970 through 2038

--- src/DateType.cc
+++ src/DateType.cc
@@ -9,37 +9,36 @@
 #include <Python.h>
 #include <datetime.h>
 
-// https://github.com/python/cpython/blob/v3.10.11/Modules/_datetimemodule.c#L89-L92
-#define DATE_SET_MICROSECOND(o, v) \
-  (((o)->data[7] = ((v) & 0xff0000) >> 16), \
-  ((o)->data[8] = ((v) & 0x00ff00) >> 8), \
-  ((o)->data[9] = ((v) & 0x0000ff)))
-
 DateType::DateType(PyObject *object) : PyType(object) {}
 
 DateType::DateType(JSContext *cx, JS::HandleObject dateObj) {
   if (!PyDateTimeAPI) { PyDateTime_IMPORT; } // for PyDateTime_FromTimestamp
 
-  // Convert by the timestamp value
   JS::Rooted<JS::ValueArray<0>> args(cx);
-  JS::Rooted<JS::Value> timeValue(cx);
-  JS_CallFunctionName(cx, dateObj, "getTime", args, &timeValue);
-  double milliseconds = timeValue.toNumber();
+  JS::Rooted<JS::Value> year(cx);
+  JS::Rooted<JS::Value> month(cx);
+  JS::Rooted<JS::Value> day(cx);
+  JS::Rooted<JS::Value> hour(cx);
+  JS::Rooted<JS::Value> minute(cx);
+  JS::Rooted<JS::Value> second(cx);
+  JS::Rooted<JS::Value> usecond(cx);
+  JS_CallFunctionName(cx, dateObj, "getUTCFullYear", args, &year);
+  JS_CallFunctionName(cx, dateObj, "getUTCMonth", args, &month);
+  JS_CallFunctionName(cx, dateObj, "getUTCDate", args, &day);
+  JS_CallFunctionName(cx, dateObj, "getUTCHours", args, &hour);
+  JS_CallFunctionName(cx, dateObj, "getUTCMinutes", args, &minute);
+  JS_CallFunctionName(cx, dateObj, "getUTCSeconds", args, &second);
+  JS_CallFunctionName(cx, dateObj, "getUTCMilliseconds", args, &usecond);
 
-  PyObject *timestampArg = PyTuple_New(2);
-  PyTuple_SetItem(timestampArg, 0, PyFloat_FromDouble(milliseconds / 1000));
-  PyTuple_SetItem(timestampArg, 1, PyDateTime_TimeZone_UTC); // Make the resulting Python datetime object timezone-aware
-                                                             // See https://docs.python.org/3/library/datetime.html#aware-and-naive-objects
-  pyObject = PyDateTime_FromTimestamp(timestampArg);
-  Py_INCREF(PyDateTime_TimeZone_UTC); // PyTuple_SetItem steals the reference
-  Py_DECREF(timestampArg);
-
-  // Round to milliseconds precision because the smallest unit for a JS Date is 1ms
-  double microseconds = PyDateTime_DATE_GET_MICROSECOND(pyObject);
-  DATE_SET_MICROSECOND(
-    (PyDateTime_DateTime *)pyObject,
-    std::lround(microseconds / 1000) * 1000
+  pyObject = PyDateTimeAPI->DateTime_FromDateAndTime(
+    year.toNumber(), month.toNumber() + 1, day.toNumber(),
+    hour.toNumber(), minute.toNumber(), second.toNumber(),
+    usecond.toNumber() * 1000,
+    PyDateTime_TimeZone_UTC, // Make the resulting Python datetime object timezone-aware
+                             // See https://docs.python.org/3/library/datetime.html#aware-and-naive-objects
+    PyDateTimeAPI->DateTimeType
   );
+  Py_INCREF(PyDateTime_TimeZone_UTC);
 }
 
 JSObject *DateType::toJsDate(JSContext *cx) {
