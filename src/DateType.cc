9e71b406f2df4df2f49523efabf84c6401b4e9c3
Tom Tang <xmader@distributive.network>
2023-07-21 03:53:04 +0000

    perf(datetime): convert Dates by timestamp value

--- src/DateType.cc
+++ src/DateType.cc
@@ -12,27 +12,18 @@
 DateType::DateType(PyObject *object) : PyType(object) {}
 
 DateType::DateType(JSContext *cx, JS::HandleObject dateObj) {
-  JS::Rooted<JS::ValueArray<0>> args(cx);
-  JS::Rooted<JS::Value> year(cx);
-  JS::Rooted<JS::Value> month(cx);
-  JS::Rooted<JS::Value> day(cx);
-  JS::Rooted<JS::Value> hour(cx);
-  JS::Rooted<JS::Value> minute(cx);
-  JS::Rooted<JS::Value> second(cx);
-  JS::Rooted<JS::Value> usecond(cx);
-  JS_CallFunctionName(cx, dateObj, "getFullYear", args, &year);
-  JS_CallFunctionName(cx, dateObj, "getMonth", args, &month);
-  JS_CallFunctionName(cx, dateObj, "getDate", args, &day);
-  JS_CallFunctionName(cx, dateObj, "getHours", args, &hour);
-  JS_CallFunctionName(cx, dateObj, "getMinutes", args, &minute);
-  JS_CallFunctionName(cx, dateObj, "getSeconds", args, &second);
-  JS_CallFunctionName(cx, dateObj, "getMilliseconds", args, &usecond);
+  if (!PyDateTimeAPI) { PyDateTime_IMPORT; } // for PyDateTime_FromTimestamp
 
-  if (!PyDateTimeAPI) { PyDateTime_IMPORT; }
-  pyObject = PyDateTime_FromDateAndTime(
-    year.toNumber(), month.toNumber() + 1, day.toNumber(),
-    hour.toNumber(), minute.toNumber(), second.toNumber(),
-    usecond.toNumber() * 1000);
+  // Convert by the timestamp value
+  JS::Rooted<JS::ValueArray<0>> args(cx);
+  JS::Rooted<JS::Value> timeValue(cx);
+  JS_CallFunctionName(cx, dateObj, "getTime", args, &timeValue);
+  double milliseconds = timeValue.toNumber();
+
+  PyObject *timestampArg = PyTuple_New(1);
+  PyTuple_SetItem(timestampArg, 0, PyFloat_FromDouble(milliseconds / 1000));
+  pyObject = PyDateTime_FromTimestamp(timestampArg);
+  Py_DECREF(timestampArg);
 }
 
 JSObject *DateType::toJsDate(JSContext *cx) {
