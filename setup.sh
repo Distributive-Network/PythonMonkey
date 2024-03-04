895f9d14c4cf879a75dde88515678b89de2d469f
Tom Tang <xmader@distributive.network>
2024-03-04 22:15:08 +0000

    ci: fix icu4c library not loaded in macos ci

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then # Linux
 elif [[ "$OSTYPE" == "darwin"* ]]; then # macOS
   brew update || true # allow failure
   brew install cmake doxygen graphviz pkg-config wget coreutils # `coreutils` installs the `realpath` command
-  brew uninstall --ignore-dependencies icu4c # https://gist.github.com/berkedel/d1fc6d13651c16002f64653096d1fded
+  brew uninstall --ignore-dependencies icu4c && brew link icu4c # https://gist.github.com/berkedel/d1fc6d13651c16002f64653096d1fded
 elif [[ "$OSTYPE" == "msys"* ]]; then # Windows
   echo "Dependencies are not going to be installed automatically on Windows."
 else
