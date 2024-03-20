27019d2e48f73acafc835cdf5642fed19a337aaa
Philippe Laporte <philippe@distributive.network>
2024-03-20 16:29:47 -0400

    added get method test

--- tests/python/test_dict_methods.py
+++ tests/python/test_dict_methods.py
@@ -444,3 +444,8 @@ def test_items_mapping():
     items = dishes.items()
     assert str(items.mapping) == "{'eggs': 2.0, 'sausage': 1.0, 'bacon': 1.0, 'spam': 500.0}"
     assert items.mapping['spam'] == 500
+
+#get method
+def test_get_method():     
+    dishes = pm.eval("({'eggs': 2, 'sausage': 1, 'bacon': 1, 'spam': 500})")
+    assert dishes.get('eggs') == 2
