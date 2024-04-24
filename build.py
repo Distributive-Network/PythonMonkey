32067373c0b3bd406a80581be831d071e6148916
Caleb Aikens <caleb@distributive.network>
2024-04-24 14:20:53 -0400

    fix(build): use multi-config generator on windows, and single- on unix

--- build.py
+++ build.py
@@ -52,10 +52,10 @@ def run_cmake_build():
 
   if platform.system() == "Windows":
     execute("cmake .. -T ClangCL", cwd=BUILD_DIR)  # use Clang/LLVM toolset for Visual Studio
+    execute(f"cmake --build . -j{CPUS} --config {build_type}", cwd=BUILD_DIR)
   else:
     execute(f"cmake -DBUILD_DOCS={build_docs} -DCMAKE_BUILD_TYPE={build_type} ..", cwd=BUILD_DIR)
-
-  execute(f"cmake --build . -j{CPUS} --config {build_type}", cwd=BUILD_DIR)
+    execute(f"cmake --build . -j{CPUS}", cwd=BUILD_DIR)
 
 
 def copy_artifacts():
