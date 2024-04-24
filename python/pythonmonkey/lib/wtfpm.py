f91b4775b6e1e5aeefb03a5123684c9f0195413c
Tom Tang <xmader@distributive.network>
2024-04-24 12:29:39 +0000

    feat: add the ability to use WTFPythonMonkey as a Python context manager (`with`-statement)

--- python/pythonmonkey/lib/wtfpm.py
+++ python/pythonmonkey/lib/wtfpm.py
@@ -12,3 +12,27 @@ def printTimersDebugInfo():
         console.log(getDebugInfo())
         console.log(new Date())
     }""")(pm.createRequire(__file__))
+
+class WTF:
+    """
+    WTFPythonMonkey to use as a Python context manager (`with`-statement)
+
+    Usage:
+        ```py
+        from pythonmonkey.lib.wtfpm import WTF
+
+        with WTF():
+            # the main entry point for the program utilizes PythonMonkey event-loop
+            asyncio.run(pythonmonkey_main())
+        ```
+    """
+    def __enter__(self):
+        pass
+    def __exit__(self, errType, errValue, traceback):
+        if errType is None: # no exception
+            return
+        elif issubclass(errType, KeyboardInterrupt): # except KeyboardInterrupt:
+            printTimersDebugInfo()
+            return True # exception suppressed
+        else: # other exceptions
+            return False
