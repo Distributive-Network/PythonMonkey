61725c644ff8d2b47d2b443869dc3c29c163642a
Tom Tang <xmader@distributive.network>
2024-05-07 20:10:39 +0000

    chore(linting): lint the newly added `wtfpm.py` file

--- python/pythonmonkey/lib/wtfpm.py
+++ python/pythonmonkey/lib/wtfpm.py
@@ -5,34 +5,38 @@
 
 import pythonmonkey as pm
 
+
 def printTimersDebugInfo():
-    pm.eval("""(require) => {
-        const internalBinding = require('internal-binding');
-        const { getAllRefedTimersDebugInfo: getDebugInfo } = internalBinding('timers');
-        console.log(getDebugInfo())
-        console.log(new Date())
-    }""")(pm.createRequire(__file__))
+  pm.eval("""(require) => {
+    const internalBinding = require('internal-binding');
+    const { getAllRefedTimersDebugInfo: getDebugInfo } = internalBinding('timers');
+    console.log(getDebugInfo())
+    console.log(new Date())
+  }""")(pm.createRequire(__file__))
+
 
 class WTF:
-    """
-    WTFPythonMonkey to use as a Python context manager (`with`-statement)
+  """
+  WTFPythonMonkey to use as a Python context manager (`with`-statement)
 
-    Usage:
-        ```py
-        from pythonmonkey.lib.wtfpm import WTF
+  Usage:
+    ```py
+    from pythonmonkey.lib.wtfpm import WTF
 
-        with WTF():
-            # the main entry point for the program utilizes PythonMonkey event-loop
-            asyncio.run(pythonmonkey_main())
-        ```
-    """
-    def __enter__(self):
-        pass
-    def __exit__(self, errType, errValue, traceback):
-        if errType is None: # no exception
-            return
-        elif issubclass(errType, KeyboardInterrupt): # except KeyboardInterrupt:
-            printTimersDebugInfo()
-            return True # exception suppressed
-        else: # other exceptions
-            return False
+    with WTF():
+      # the main entry point for the program utilizes PythonMonkey event-loop
+      asyncio.run(pythonmonkey_main())
+    ```
+  """
+
+  def __enter__(self):
+    pass
+
+  def __exit__(self, errType, errValue, traceback):
+    if errType is None:  # no exception
+      return
+    elif issubclass(errType, KeyboardInterrupt):  # except KeyboardInterrupt:
+      printTimersDebugInfo()
+      return True  # exception suppressed
+    else:  # other exceptions
+      return False
