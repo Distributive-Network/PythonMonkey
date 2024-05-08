2b99eebdd172941f3a3b86e0647acef2f0f7e1d4
Tom Tang <xmader@distributive.network>
2024-05-08 20:50:13 +0000

    fix(pmpm): fix importing pmpm for post-install-hook

--- python/pminit/post-install-hook.py
+++ python/pminit/post-install-hook.py
@@ -1,27 +1,12 @@
 import os
-import sys
-import importlib
-import importlib.util
+import pmpm
 
 WORK_DIR = os.path.join(
   os.path.realpath(os.path.dirname(__file__)),
   "pythonmonkey"
 )
 
-def import_file(module_name: str, file_path: str):
-  """
-  Import a Python file from its relative path directly.
-
-  See https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
-  """
-  spec = importlib.util.spec_from_file_location(module_name, file_path)
-  module = importlib.util.module_from_spec(spec) # type: ignore
-  sys.modules[module_name] = module
-  spec.loader.exec_module(module) # type: ignore
-  return module
-
 def main():
-  pmpm = import_file("pmpm", os.path.join(os.path.dirname(__file__), "./pmpm.py")) # from . import pmpm
   pmpm.main(WORK_DIR) # cd pythonmonkey && npm i
 
 if __name__ == "__main__":
