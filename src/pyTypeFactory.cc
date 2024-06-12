91355f225f67fa8284a92a8614533f0e5cff6b00
Tom Tang <xmader@distributive.network>
2024-06-12 10:25:31 +0000

    fix(pyTypeFactory): we shouldn't use `printf` here. Print to the Python error message instead.

--- src/pyTypeFactory.cc
+++ src/pyTypeFactory.cc
@@ -37,6 +37,8 @@
 #include <js/ValueArray.h>
 
 PyObject *pyTypeFactory(JSContext *cx, JS::HandleValue rval) {
+  std::string errorString;
+
   if (rval.isUndefined()) {
     return NoneType::getPyObject();
   }
@@ -53,7 +55,7 @@ PyObject *pyTypeFactory(JSContext *cx, JS::HandleValue rval) {
     return StrType::getPyObject(cx, rval);
   }
   else if (rval.isSymbol()) {
-    printf("symbol type is not handled by PythonMonkey yet\n");
+    errorString = "symbol type is not handled by PythonMonkey yet.\n";
   }
   else if (rval.isBigInt()) {
     return IntType::getPyObject(cx, rval.toBigInt());
@@ -117,10 +119,10 @@ PyObject *pyTypeFactory(JSContext *cx, JS::HandleValue rval) {
     return DictType::getPyObject(cx, rval);
   }
   else if (rval.isMagic()) {
-    printf("magic type is not handled by PythonMonkey yet\n");
+    errorString = "magic type is not handled by PythonMonkey yet.\n";
   }
 
-  std::string errorString("pythonmonkey cannot yet convert Javascript value of: ");
+  errorString += "pythonmonkey cannot yet convert Javascript value of: ";
   JSString *valToStr = JS::ToString(cx, rval);
   if (!valToStr) { // `JS::ToString` returns `nullptr` for JS symbols, see https://hg.mozilla.org/releases/mozilla-esr102/file/3b574e1/js/src/vm/StringType.cpp#l2208
     // TODO (Tom Tang): Revisit this once we have Symbol coercion support
