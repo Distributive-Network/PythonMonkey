778ca86797c12498bd98d10f85f774e6502d413a
Caleb Aikens <caleb@distributive.network>
2024-08-26 10:55:04 -0400

    fix(Py<List/Object>ProxyHandler): don't finalize if python itself is finalizing

--- src/PyObjectProxyHandler.cc
+++ src/PyObjectProxyHandler.cc
@@ -87,8 +87,8 @@ void PyObjectProxyHandler::finalize(JS::GCContext *gcx, JSObject *proxy) const {
   // We cannot call Py_DECREF here when shutting down as the thread state is gone.
   // Then, when shutting down, there is only on reference left, and we don't need
   // to free the object since the entire process memory is being released.
-  PyObject *self = JS::GetMaybePtrFromReservedSlot<PyObject>(proxy, PyObjectSlot);
-  if (Py_REFCNT(self) > 1) {
+  if (!_Py_IsFinalizing()) {
+    PyObject *self = JS::GetMaybePtrFromReservedSlot<PyObject>(proxy, PyObjectSlot);
     Py_DECREF(self);
   }
 }
