2f30f991eb199efdeffb1988482d1f1832722553
Philippe Laporte <philippe@distributive.network>
2024-09-17 11:44:27 -0400

    cleanup unused code

--- src/JSArrayProxy.cc
+++ src/JSArrayProxy.cc
@@ -1178,31 +1178,12 @@ extern PyObject *const *_PyArg_UnpackKeywords(
 );
 
 PyObject *JSArrayProxyMethodDefinitions::JSArrayProxy_sort(JSArrayProxy *self, PyObject *const *args, Py_ssize_t nargs, PyObject *kwnames) {
-  #if defined(Py_BUILD_CORE) && !defined(Py_BUILD_CORE_MODULE)
-
-  #define NUM_KEYWORDS 2
-  static struct {
-    PyGC_Head _this_is_not_used;
-    PyObject_VAR_HEAD
-    PyObject *ob_item[NUM_KEYWORDS];
-  } _kwtuple = {
-    .ob_base = PyVarObject_HEAD_INIT(&PyTuple_Type, NUM_KEYWORDS)
-    .ob_item = {&_Py_ID(key), &_Py_ID(reverse), },
-  };
-  #undef NUM_KEYWORDS
-  #define KWTUPLE (&_kwtuple.ob_base.ob_base)
-
-  #else // !Py_BUILD_CORE
-  #  define KWTUPLE NULL
-  #endif // !Py_BUILD_CORE
-
   static const char *const _keywords[] = {"key", "reverse", NULL};
   static _PyArg_Parser _parser = {
     .keywords = _keywords,
     .fname = "sort",
-    .kwtuple = KWTUPLE,
+    .kwtuple = NULL,
   };
-  #undef KWTUPLE
 
   PyObject *argsbuf[2];
   Py_ssize_t noptargs = nargs + (kwnames ? PyTuple_GET_SIZE(kwnames) : 0) - 0;
